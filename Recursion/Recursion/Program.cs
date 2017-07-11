0b8dae073d635497905fab2924c4a006fe0071aa
Andre Murrell <andre.murrell@outlook.com>
2017-07-11 10:05:26 -0400

    Add files via upload

---
+++ Recursion/Recursion/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Recursion
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int practiceNumber = 100;
+            recursivePractice(practiceNumber);
+        }
+
+        private static void recursivePractice(int practiceNumber)
+        {
+            
+        }
+    }
+}
